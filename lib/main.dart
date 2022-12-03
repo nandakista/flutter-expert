371ab2244d0cd3cc69c3988e0c3cd5e9e3f14b5e
nandakista <nanda.kista@gmail.com>
2022-12-03 13:10:54 +0700

    Integrate Firebase into project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:submission/core/theme/app_theme.dart';
 import 'package:provider/provider.dart';
@@ -29,9 +30,13 @@ import 'package:submission/ui/views/watchlist/watchlist_view.dart';
 import 'package:submission/ui/views/watchlist/tv/watchlist_tv_view.dart';
 
 import 'core/route_observer.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await di.init();
   runApp(const MyApp());
 }
--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:submission/ui/views/home/components/now_playing_components.dart';
